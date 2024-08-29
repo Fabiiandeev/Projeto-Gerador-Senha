9eaba8411b1caca02829a22975834c3a22c0d448
Fabiiandeev <lipiph21@gmail.com>
2024-08-29 15:37:33 -0300

    alterações script, gerar senha,  copypassword

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
+let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$*";
+let novaSenha= "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    let pass = "";
+    for(i=0, n = charset.length;  i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random()*n));
+        
+    }
+    alert("AGUARDE VAMOS GERAR SUA SENHA ....")
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+    
+
+}
+
+function copyPassword(){
+    navigator.clipboard.writeText(novaSenha);
+    alert("SENHA COPIADA COM SUCESSO!")
+    
+}
+
